f03b13f32cf4b56003f6d94a24fb28ebfc28845e
EC2 Default User <ec2-user@ip-172-31-30-39.ec2.internal>
2022-06-01 21:32:31 +0000

    modulated receipt + added new db

--- helpers.py
+++ helpers.py
@@ -1,26 +1,20 @@
-#helpful imports
-import json
-from decimal import Decimal
-
 #database import 
 import boto3
-
 #database setup
 dynamo = boto3.resource("dynamodb")
-
 #TABLES:
 menu = dynamo.Table('Menu')
 order = dynamo.Table('Order') #for open orders
 history = dynamo.Table('History') #for receipts (old order)
+info = dynamo.Table('Info')
 
-#FOR THE MENU
+#MENU TABLE
 #add json to database
 def addMenu(item):
     #put the item in
     menu.put_item(
         Item=item
     )
-
 #remove from database
 def removeMenu(key):
     menu.delete_item(
@@ -28,7 +22,6 @@ def removeMenu(key):
             'RestaurantId': key
         }
     )
-
 #update menu item on database
 def updateMenu(key, newMenu):
     #update the item
@@ -41,7 +34,6 @@ def updateMenu(key, newMenu):
             ':val1': newMenu
         }
     )
-
 #retreive from database
 def retreiveMenu(key):
     return menu.get_item(
@@ -51,24 +43,17 @@ def retreiveMenu(key):
     )["Item"]
 
 
-
-#FOR THE ORDER TABLE (move to another script?)
-#add an order (first time)
+#ORDER TABLE
+#add an order to processing queue
 def addOrder( orderInfo ):
     order.put_item(
         Item=orderInfo
     )
-
-#update an order (append)
-#in the future we need a solution for multiple of the same item
+#update an existing order -- in the future we need a solution for multiple of the same item
 def updateOrder( orderId, orderItems ):
-    #get the key
-    key={
-        'OrderId': orderId
-    }
+    key={'OrderId': orderId}
     #get the old OrderItems
     items = order.get_item(Key=key)["Item"]["OrderItems"]
-
     #create a dictionary to map ItemId : index for every item currently in the order
     name_index = { items[index]["ItemId"]:index for index in range(len(items)) }
     #loop through all the "new" items and merge if possible, else just add
@@ -79,7 +64,6 @@ def updateOrder( orderId, orderItems ):
             items[ name_index[name] ]["Quantity"] += amount
         else:
             items += [item]
-
     #update the order
     order.update_item(
         Key=key,
@@ -88,9 +72,7 @@ def updateOrder( orderId, orderItems ):
             ':val1': items
         }
     )
-
-#get all the orders in the Order table
-#what order should it return items in?
+#return all orders in the Order table (processing queue) (in the future come up with an order)
 def getOrders():
     response = order.scan()
     data = response["Items"]
@@ -98,59 +80,47 @@ def getOrders():
         response = table.scan( ExclusiveStartKey=response["LastEvaluatedKey"] )
         data.extend(response["Items"])
     return data
-
 #retreive an order
 def retreiveOrder( orderId ):
     key = {
         "OrderId": orderId
     }
     return order.get_item( Key=key )["Item"]
-
 #to remove an item from order table
 def removeOrder( orderId ):
     key = {
         "OrderId": orderId
     }
     order.delete_item( Key=key )
-
-#returns how the receipt would look like at the current state of the order
+#returns the current state of receipt
 def getReceipt( orderId ):
-    #make the base for the receipt the order 
     receipt = retreiveOrder( orderId )    
-
-    #get the menu of the restaurant
     menu = retreiveMenu( receipt["RestaurantId"] )
-    #build a dict to keep track of itemId : price  
+    zipCode = retreiveInfo( receipt["RestaurantId"] )["ZipCode"]
+    #build a dict to keep track of {itemId : price}  
     item_map = {}
     for category in menu["Categories"]:
         for item in category["MenuItems"]:
             itemId = item["Id"]
             price = float( item["Price"] )
             item_map[ itemId ] = price
-    
-    #in the future make an API call to get the tax rate... for now I am using generic taxrate 
-    #state = menu["ZipCode"]
-    tax_rate = 8.875 #in percent
     total = 0.0
-
-    #loop through all the OrderItems and add a price field
+    #calculate subtotals for every item
     for item in receipt["OrderItems"]:
         quantity = int( item["Quantity"] )
         price = item_map[ item["ItemId"] ]
-        #calculate subtotal and update total 
         subtotal = quantity * price 
         total += subtotal
         item["Subtotal"] = str(subtotal)
-    
-    #calculate tax
+    from tax import get_tax_rate
+    tax_rate = get_tax_rate(zipCode)
     total *= ( 1 + (tax_rate/100) )
-    #add the price + tax rate into total
     receipt["Total"] = str( round(total, 2) )
     receipt["TaxRate"] = str( tax_rate )
     return receipt
 
 
-#FOR THE HISTORY TABLE
+#HISTORY TABLE
 #add an item to the history table
 def addHistory( order ):
     history.put_item(
@@ -158,15 +128,32 @@ def addHistory( order ):
     )
 
 
-
-#USE BOTH HISTORY AND ORDER TABLES
-#also ADD RECEIPT GENERATION
-#complete an order
+#HISTORY AND ORDER
+#complete an order (take from processing queue and put it in History)
 def closeOrder( orderId ):
     receipt = getReceipt( orderId )     
-    #update the dbs
     addHistory( receipt )
     removeOrder( orderId )    
-    #return the receipt
     return receipt
 
+
+#FOR THE INFO TABLE
+#wouldnt this be better in an sql db?
+#add info to database
+def addInfo( newInfo ):
+    #update the item
+    info.add_item( newInfo )
+#remove from database
+def removeInfo( restaurantId ):
+    info.delete_item(
+        Key={
+            'RestaurantId': restaurantId
+        }
+    )
+#retreive from database
+def retreiveInfo( restaurantId ):
+    return info.get_item(
+        Key={
+            'RestaurantId': restaurantId
+        }
+    )["Item"]
---
+++ tax.py
@@ -0,0 +1,3 @@
+#currently a stub
+def get_tax_rate( zipCode ):
+    return 8.875
